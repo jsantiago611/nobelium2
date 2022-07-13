ee572b253eb49a4bc5db935044422f004ad4e48d
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-07-13 17:31:54 -0400

    update fonts again

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,8 @@ module.exports = {
         }
       },
       fontFamily: {
-        sans: ['"GT-America"', ...fontFamily.sans, ...fontSansCJK],
-        serif: ['"Bradford"', ...fontFamily.serif, ...fontSerifCJK],
+        sans: ['"Polysans"', ...fontFamily.sans, ...fontSansCJK],
+        serif: ['"GT-Super"', ...fontFamily.serif, ...fontSerifCJK],
         mono: ['"GT-America"', ...fontFamily.sans, ...fontSansCJK],
         noEmoji: [
           '"IBM Plex Sans"',
