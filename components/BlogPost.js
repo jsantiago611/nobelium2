afc999a211dcc413a0d8af8ce58d0eb7f6b578e9
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-07-20 15:08:26 -0400

    leading override feed summary

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -16,7 +16,7 @@ const BlogPost = ({ post }) => {
             </time>
           </header>
           <main>
-            <p className="text-lg md:text-lg hidden md:block leading-5 text-gray-700 dark:text-gray-300 font-serif font-light">
+            <p className="text-lg md:text-lg leading-5 md:leading-5 hidden md:block text-gray-700 dark:text-gray-300 font-serif font-light">
               {post.summary}
             </p>
           </main>
