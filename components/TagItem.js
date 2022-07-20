9b239fd3d70161ec1847b3ef99a3e0d6e83e5a04
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-07-20 16:27:52 -0400

    tagitem on post to 1rem

--- components/TagItem.js
+++ components/TagItem.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const TagItem = ({ tag }) => (
   <Link href={`/tag/${encodeURIComponent(tag)}`}>
     <a>
-      <p className="mr-1 rounded-full px-2 py-2 border leading-none text-sm dark:border-gray-600">
+      <p className="mr-1 rounded-full px-2 py-2 border leading-none text-md dark:border-gray-600">
         {tag}
       </p>
     </a>
