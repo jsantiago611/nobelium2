bd1c75d425500ae09ce9ef9636eb9390879ed071
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-07-20 12:47:31 -0400

    post summary leading-6

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -16,7 +16,7 @@ const BlogPost = ({ post }) => {
             </time>
           </header>
           <main>
-            <p className="hidden md:block leading-7 text-gray-700 dark:text-gray-300 font-serif font-light">
+            <p className="hidden md:block leading-6 text-gray-700 dark:text-gray-300 font-serif font-light">
               {post.summary}
             </p>
           </main>
