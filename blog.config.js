7088990fbcdb3dfb7a063d56af5a4d9b60f88119
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-07-21 16:07:27 -0400

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#FEFEFE', // use hex value, don't forget '#' e.g #fffefc
+  lightBackground: '#F5F4F1', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#0B2823', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: '', // If leave this empty, current year will be used.
