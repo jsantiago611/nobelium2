44282c6ca6f2806ed5caa0527c0d20fb6a985cb2
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-08-02 14:50:17 -0400

    comment out the blog author name

--- layouts/layout.js
+++ layouts/layout.js
@@ -45,7 +45,7 @@ const Layout = ({
                   src={`https://gravatar.com/avatar/${emailHash}`}
                   className="rounded-full"
                 />
-                <p className="ml-2 md:block font-serif font-medium">{BLOG.author}</p>
+                // <p className="ml-2 md:block font-serif font-medium">{BLOG.author}</p>
               </a>
               <span className="block">&nbsp;/&nbsp;</span>
             </div>
