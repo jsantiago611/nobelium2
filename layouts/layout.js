ef05126893f0355408ba97e5e1ee9633a8dc25bf
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-11-01 17:50:39 -0400

    altering font for notes

--- layouts/layout.js
+++ layouts/layout.js
@@ -31,17 +31,17 @@ const Layout = ({
     >
       <article>
         {frontMatter.tags && (
-              <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags font-mono justify-center border-none uppercase">
+              <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags font-mono justify-center border-none">
                 {frontMatter.tags.map(tag => (
                   <TagItem key={tag} tag={tag} />
                 ))}
               </div>)}
-        <h1 className="font-medium text-5xl md:text-7xl text-black dark:text-white font-mono tracking-tight text-center regular">
+        <h1 className="font-light text-5xl md:text-7xl text-black dark:text-white font-serif tracking-tight text-center regular">
           {frontMatter.title}
         </h1>
         {frontMatter.type[0] !== 'Page' && (
           <nav className="flex mt-7 text-gray-500 dark:text-gray-400 solid border-y-2 border-gray-200 mb-4 justify-center">
-            <div className="mr-2 md:ml-0 font-mono font-thin inline-flex space-x-1">
+            <div className="mr-2 md:ml-0 font-mono font-regular inline-flex space-x-1">
               <p>Last Updated on</p>
               <p> </p>
               {formatDate(
