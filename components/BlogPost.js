98cf8d7488c94477873bba2f261237b5cb781465
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2022-11-02 10:36:09 -0400

    smaller note description

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -16,7 +16,7 @@ const BlogPost = ({ post }) => {
             </time>
           </header>
           <main>
-            <p className="text-3xl md:text-4xl tracking-tighter hidden md:block text-gray-500 dark:text-gray-300 font-serif lowercase font-light pb-8 border-solid border-gray-300 border-y">
+            <p className="text-2xl md:text-3xl tracking-tighter hidden md:block text-gray-500 dark:text-gray-300 font-serif lowercase font-light pb-8 border-solid border-gray-300 border-y">
               {post.summary}
             </p>
           </main>
