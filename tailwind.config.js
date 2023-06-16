0430cd8b63c43a3d876efe8cb637ce448d873599
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2023-06-16 14:45:49 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,8 @@ module.exports = {
         }
       },
       fontFamily: {
-        sans: ['"Labilvar"', ...fontFamily.sans, ...fontSansCJK],
-        serif: ['"Bradford"', ...fontFamily.serif, ...fontSerifCJK],
+        sans: ['"Bradford"', ...fontFamily.sans, ...fontSansCJK],
+        serif: ['"Labilvar"', ...fontFamily.serif, ...fontSerifCJK],
         mono: ['"Bradford"', ...fontFamily.sans, ...fontSansCJK],
         display: ['"BW"', ...fontFamily.sans, ...fontSansCJK],
         noEmoji: [
