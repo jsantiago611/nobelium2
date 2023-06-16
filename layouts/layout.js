6f8b619b2b5fdb4e3e6694fe8e3846bc305b901f
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2023-06-16 15:07:48 -0400

    Update layout.js

--- layouts/layout.js
+++ layouts/layout.js
@@ -31,7 +31,7 @@ const Layout = ({
       fullWidth={fullWidth}
     >
       <article>
-        <h1 className="font-serif text-5xl md:text-6xl text-black dark:text-white tracking-tighter regular">
+        <h1 className="font-serif text-5xl md:text-6xl text-black dark:text-white tracking-tighter">
           {frontMatter.title}
         </h1>
         {frontMatter.type[0] !== 'Page' && (
