6cd9afa143a2adb043e4e28f0f7a175fee23e643
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2023-06-16 14:11:26 -0400

    Update BlogPost.js

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -8,7 +8,7 @@ const BlogPost = ({ post }) => {
       <a>
         <article key={post.id} className="mb-6 md:mb-8">
           <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-            <h2 className="text-4xl md:text-5xl tracking-tighter mb-2 cursor-pointer text-black dark:text-gray-100 font-sans regular hover:underline transition-all" style="font-variation-settings: 'wght' 30, 'stbl' 100;">
+            <h2 className="text-4xl md:text-5xl tracking-tighter mb-2 cursor-pointer text-black dark:text-gray-100 font-sans regular hover:underline transition-all">
               {post.title}
             </h2>
             <time className="flex-shrink-0 text-gray-400 dark:text-gray-400 font-mono font-normal text-sm">
